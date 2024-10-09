839604fb3f73aecb8ed3bd362634f90d4b9d4f36
David <sumbulovich@gmail.com>
2024-10-09 12:13:17 +0200

    no message

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,21 @@
 import app from "./app";
+import { setupWebSocketServer } from "./routes/websocket";
 
 const port = process.env['PORT'] || 3000;
 
 // Start up the Node server
 const server = app;
 
-server.get('/', function (req, res) {
+server.get('/', (req, res) => {
   res.send('Hello World!')
 });
 
 server.listen(port, () => {
   console.log(`Node Express server listening on http://localhost:${port}`);
 });
+
+// Set up the WebSocket server
+setupWebSocketServer(3001);
 
 export default server
 
